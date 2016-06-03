61b674a4f5017242a36982f327f4fb5556f2b273
swc8912 <swc8912@genikids.com>
2016-06-03 13:02:11 +0900

    deque 코드

---
+++ Fri/20160603/Deque.h
@@ -0,0 +1,103 @@
+#pragma once
+#include <cstdlib>
+
+class Deque
+{
+private:
+	typedef struct Node
+	{
+		int data;
+		Node* prev;
+		Node* next;
+	} Node;
+
+	Node* head;
+	Node* tail;
+
+public:
+	Deque()
+	{
+		head = NULL;
+		tail = NULL;
+	}
+
+	Node* createNode(int data)
+	{
+		Node* newNode = (Node*)malloc(sizeof(Node));
+		newNode->data = data;
+		if (head == NULL)
+			head = newNode;
+		if (tail == NULL)
+			tail = head;
+
+		head->prev = NULL;
+		tail->next = NULL;
+		return newNode;
+	}
+
+	void addFront(Node* node)
+	{
+		if (head != NULL)
+		{
+			head->prev = node;
+			node->next = head;
+			head = node;
+		}
+	}
+
+	void addBack(Node* node)
+	{
+		if (tail != NULL)
+		{
+			tail->next = node;
+			node->prev = tail;
+			tail = node;
+		}
+	}
+
+	Node* removeFront()
+	{
+		if (head != NULL)
+		{
+			Node* temp = head;
+			head = head->next;
+			if (head == tail)
+				tail = NULL;
+			free(temp);
+		}
+	}
+
+	Node* getFront()
+	{
+		if (head != NULL)
+		{
+			return head;
+		}
+	}
+
+	void removeBack()
+	{
+		if (tail != NULL)
+		{
+			Node* temp = tail;
+			tail = tail->prev;
+			if(head == tail)
+				head = NULL;
+			free(temp);
+		}
+	}
+
+	Node* getBack()
+	{
+		if (tail != NULL)
+		{
+			return tail;
+		}
+	}
+
+	bool isEmpty()
+	{
+		return head == NULL && tail == NULL ? true : false;
+	}
+};
+
